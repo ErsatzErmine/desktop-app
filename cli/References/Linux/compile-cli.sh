43769f0ff0a3a637480bd6fee701ca9b99d72284
ErsatzErmine <26928527+ErsatzErmine@users.noreply.github.com>
2026-07-17 22:36:07 +0000

    Remove git hash check from compile-cli.sh

--- cli/References/Linux/compile-cli.sh
+++ cli/References/Linux/compile-cli.sh
@@ -34,7 +34,7 @@ mkdir -p $OUT_DIR
 # version info variables
 VERSION=""
 DATE="$(date "+%Y-%m-%d")"
-COMMIT="$(git rev-list -1 HEAD)"
+COMMIT="xxxx"
 
 # reading version info from arguments
 while getopts ":v:" opt; do
