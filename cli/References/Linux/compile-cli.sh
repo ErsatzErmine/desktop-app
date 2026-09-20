c727d1278d0443a3f70e26b1d8b24b4fc24d5d02
ErsatzErmine <26928527+ErsatzErmine@users.noreply.github.com>
2026-09-20 16:05:06 -0600

    Update compile-cli.sh

--- cli/References/Linux/compile-cli.sh
+++ cli/References/Linux/compile-cli.sh
@@ -34,7 +34,7 @@ mkdir -p $OUT_DIR
 # version info variables
 VERSION=""
 DATE="$(date "+%Y-%m-%d")"
-COMMIT="$(git rev-list -1 HEAD)"
+COMMIT="Sept0326"
 
 # reading version info from arguments
 while getopts ":v:" opt; do
